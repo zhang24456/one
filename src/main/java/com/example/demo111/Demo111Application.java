a0509060ad04a3251dfdf6241503da8eb20e5b75
zhang24456 <2445635335@qq.com>
2018-12-04 14:39:49 +0800

    zj

---
+++ src/main/java/com/example/demo111/Demo111Application.java
@@ -0,0 +1,12 @@
+package com.example.demo111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo111Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo111Application.class, args);
+    }
+}
